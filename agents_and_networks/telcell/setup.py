88cd00a80204966f69ab479b9a54311bcd42771c
Vincent Engbers <vincent.engbers@gmail.com>
2024-04-12 11:18:57 +0200

    Speed is now also saved.

---
+++ agents_and_networks/telcell/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="telcell",
+    packages=find_packages(),
+    version="0.0.2",
+    description="telcell",
+    author="NFI",
+    license="",
+)
